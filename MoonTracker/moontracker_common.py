65420b8370fd0a188df5198bcfad36fb6d45bd90
AyhamRatrout <aiham2000.rami@gmail.com>
2023-04-12 16:31:53 -0400

    Begin adding the command line utility back

---
+++ MoonTracker/moontracker_common.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client
+
+# MQTT Topic Names
+TOPIC_SET_TRACKER_CONFIG = "moontracker/set_config"
+TOPIC_TRACKER_CHANGE_NOTIFICATION = "moontracker/changed"
+
+# MQTT Broker Connection info
+MQTT_VERSION = paho.mqtt.client.MQTTv311
+MQTT_BROKER_HOST = "localhost"
+MQTT_BROKER_PORT = 1883
+MQTT_BROKER_KEEP_ALIVE_SECS = 60
