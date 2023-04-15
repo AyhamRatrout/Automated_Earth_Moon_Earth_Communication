cc9b5f5e12d16437a37d06db55f8f8871032973f
AyhamRatrout <aiham2000.rami@gmail.com>
2023-04-15 14:42:45 -0400

    Finish service skeleton - need to implement hardware updates

--- MoonTracker/moontracker_service.py
+++ MoonTracker/moontracker_service.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
 import json
 import pigpio
 import shelve
 import platform
+import math
+from threading import Thread
 import paho.mqtt.client as mqtt
 from moontracker_common import *
+
 
 if 'arm' not in platform.platform():
     class MoonTrackerAzimuthDriver(object):
@@ -37,9 +41,9 @@ class MoonTrackerService(object):
         self.db = shelve.open(MOONTRACKER_STATE_FILENAME, writeback=True)
 
         if 'azimuth' not in self.db:
-            self.db['azimuth'] = 0.0 #pull from library here
+            self.db['azimuth'] = round(0.0, FP_DIGITS) # pull from library here
         if 'elevation' not in self.db:
-            self.db['elevation'] = 0.0 #pull from library here
+            self.db['elevation'] = round(0.0, FP_DIGITS) # pull from library here
         if 'on' not in self.db:
             self.db['on'] = True
         if 'client' not in self.db:
@@ -67,14 +71,63 @@ class MoonTrackerService(object):
 
     def default_on_message(self, client, userdata, msg):
         print("Received unexpected message on topic " +
-              msg.topic + " with payload '" + str(msg.payload) + "'")
+              msg.topic + " with payload {" + str(msg.payload) + "}")
 
     def on_message_set_config(self, client, userdata, msg):
         try:
+            new_config = json.loads(msg.payload.decode('utf-8'))
+            if 'client' in new_config:
+                self.set_last_client(new_config['client'])
+            if 'on' in new_config:
+                self.set_current_onoff_status(new_config['on'])
+            if 'azimuth' in new_config:
+                self.set_current_azimuth(new_config['azimuth'])
+            if 'elevation' in new_config:
+                self.set_current_elevation(new_config['elevation'])
+            self.publish_config_change()
         except InvalidMoonTrackerConfig:
-            print("" + str(msg.payload))
+            print("Error applying new settings: " + str(msg.payload))
 
     def publish_config_change(self):
+        config = {'azimuth': self.get_current_azimuth(),
+                    'elevation': self.get_current_elevation(),
+                    'on': self.get_current_onoff_status(),
+                    'client': self.get_last_client()}
+        self._client.publish(TOPIC_SET_TRACKER_CONFIG,
+                            json.dumps(config).encode('utf-8'), retain=True)
+
+    def get_last_client(self):
+        return self.db['client']
+
+    def set_last_client(self, new_client):
+        self.db['client'] = new_client
+
+    def get_current_azimuth(self):
+        return round(self.db['azimuth'], FP_DIGITS)
+
+    def set_current_azimuth(self, new_azimuth):
+        if new_azimuth < 0.0 or new_azimuth > 360.0:
+            raise InvalidMoonTrackerConfig()
+        self.db['azimuth'] = round(new_azimuth, FP_DIGITS)
+        # update hardware here in a new thread
+
+    def get_current_elevation(self):
+        return round(self.db['elevation'], FP_DIGITS)
+
+    def set_current_elevation(self, new_elevation):
+        if new_elevation < 0.0 or new_elevation > 180.0:
+            raise InvalidMoonTrackerConfig()
+        self.db['elevation'] = round(new_elevation, FP_DIGITS)
+        # update hardware here in a different thread
+
+    def get_current_onoff_status(self):
+        return self.db['on']
+
+    def set_current_onoff_status(self, new_onoff_status):
+        if new_onoff_status not in [True, False]:
+            raise InvalidMoonTrackerConfig()
+        self.db['on'] = new_onoff_status
+        # update hardware here in a new thread
 
 
 if __name__ == '__main__':
